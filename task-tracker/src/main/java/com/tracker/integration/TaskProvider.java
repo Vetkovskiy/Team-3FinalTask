e286867260413ee51e5916df5f650ce5c00ab118
MadKenlok <162867816+MadKenlok@users.noreply.github.com>
2025-09-13 12:53:23 +0300

    Create TaskProvider.java

---
+++ task-tracker/src/main/java/com/tracker/integration/TaskProvider.java
@@ -0,0 +1,27 @@
+package com.tracker.integration;
+
+import java.util.List;
+
+/**
+ * Интерфейс для получения задач от модуля коллекций (Владимир)
+ * Позволяет интегрироваться с кастомной коллекцией TaskList
+ */
+public interface TaskProvider {
+    /**
+     * Получить все задачи
+     * @return список всех задач
+     */
+    List<Object> getAllTasks();
+    
+    /**
+     * Получить количество задач
+     * @return количество задач
+     */
+    int getTaskCount();
+    
+    /**
+     * Проверить, пуста ли коллекция
+     * @return true если коллекция пуста
+     */
+    boolean isEmpty();
+}
