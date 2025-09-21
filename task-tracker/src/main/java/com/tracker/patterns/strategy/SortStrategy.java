490ac818a3bd2f18f849b9546f76769d9ab3cf76
MadKenlok <162867816+MadKenlok@users.noreply.github.com>
2025-09-22 02:03:10 +0300

    Delete SortStrategy.java

--- task-tracker/src/main/java/com/tracker/patterns/strategy/SortStrategy.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.tracker.patterns.strategy;
-
-import java.util.Comparator;
-import java.util.List;
-
-public interface SortStrategy<T> {
-    void sort(List<T> list, Comparator<T> comparator);
-}
