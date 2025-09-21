f3b021f9164080ec4fca620121e36792572d8424
DmitriiG <dmitrii.gulco@gmail.com>
2025-09-21 22:26:00 +0300

    Реализовал метод записи в файл и тесты к нему

--- src/test/java/com/tracker/integration/FileDataProviderImplTest.java
+++ src/test/java/com/tracker/integration/FileDataProviderImplTest.java
@@ -1,9 +1,14 @@
 package com.tracker.integration;
 
 import com.tracker.collection.Task;
+import com.tracker.exception.FileProcessingException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -87,5 +92,92 @@ class FileDataProviderImplTest {
         List<Task> tasks = provider.loadFromFile(filePath);
         //Then
         assertEquals(1, tasks.size());
+    }
+
+    @Test
+    @DisplayName("Should return true when list is empty")
+    void test8() {
+        //Given
+        String filePath = "src/test/resources/Tasks8.csv";
+        createEmptyFile(filePath);
+        //When
+        boolean result = provider.saveToFile(filePath, List.of());
+        //Then
+        assertTrue(result);
+        deleteFile(filePath);
+    }
+
+    @Test
+    @DisplayName("Should return true when list has one item")
+    void test9() {
+        //Given
+        String filePath = "src/test/resources/Tasks8.csv";
+        createEmptyFile(filePath);
+        Task task = new Task(1,"Test Title", "Test Descr", Task.Priority.HIGH, Task.Status.NEW, LocalDate.now());
+        //When
+        boolean result = provider.saveToFile(filePath, List.of(task));
+        //Then
+        assertTrue(result);
+        deleteFile(filePath);
+    }
+
+    @Test
+    @DisplayName("Should return true when list has multiple items")
+    void test10() {
+        //Given
+        String filePath = "src/test/resources/Tasks8.csv";
+        createEmptyFile(filePath);
+        Task task1 = new Task(1,"Test Title", "Test Descr", Task.Priority.HIGH, Task.Status.NEW, LocalDate.now());
+        Task task2 = new Task(1,"Test Title", "Test Descr", Task.Priority.HIGH, Task.Status.NEW, LocalDate.now());
+        Task task3 = new Task(1,"Test Title", "Test Descr", Task.Priority.HIGH, Task.Status.NEW, LocalDate.now());
+        //When
+        boolean result = provider.saveToFile(filePath, List.of(task1, task2, task3));
+        //Then
+        assertTrue(result);
+        deleteFile(filePath);
+    }
+
+    @Test
+    @DisplayName("Should return false")
+    void test11() {
+        //Given
+        String filePath = "src/test/resources/";
+        Task task = new Task(1,"Test Title", "Test Descr", Task.Priority.HIGH, Task.Status.NEW, LocalDate.now());
+        //When
+        boolean result = provider.saveToFile(filePath, List.of(task));
+        //Then
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("Should throw FileProcessingException when file doesn't exist")
+    void test12() {
+        assertThrows(FileProcessingException.class, new MyExecutable() );
+    }
+
+    class MyExecutable implements Executable {
+        @Override
+        public void execute() throws Throwable {
+            //Given
+            String filePath = "src/test/resources/Tasks404.csv";
+            //When
+            provider.loadFromFile(filePath);
+        }
+    }
+
+
+    private void deleteFile(String filePath) {
+        File file = new File(filePath);
+        file.delete();
+    }
+
+    private void createEmptyFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            file.createNewFile();
+            
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
